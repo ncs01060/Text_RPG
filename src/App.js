65cc1a8bebc6f57fa25c349ca06e0820173b8f51
이영민 <>
2024-05-16 16:04:35 +0900

    youngminSettings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Youngmin_index from "./youngmin/youngmin_index";
+import Jewon_index from "./jewon/jewon_index";
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      <h1>text-rpg1</h1>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Youngmin_index />} path="/youngminMain"></Route>
+          <Route element={<Jewon_index />} path="/jewonMain"></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/youngmin/youngmin_index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const youngmin_index = () => {
+  return (
+    <div className="flex flex-row p-4 m-6 ">
+      <div className="bg-green-500 p-4 m-6 rounded text-white">
+        <h1 className="text-blue-900">Proj.1</h1>
+        <h2>살려줘요 여기 사람이 갇혀있어요</h2>
+      </div>
+      <div className="bg-green-500 p-4 m-6 rounded text-white">
+        <h1>Proj.2</h1>
+      </div>
+      <div className="bg-green-500 p-4 m-6 rounded text-white">
+        <h1>Proj.3</h1>
+      </div>
+    </div>
+  );
+};
+
+export default youngmin_index;
