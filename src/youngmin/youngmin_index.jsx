e5710fa817800ffc2728d377f3bcc4803b215468
이영민 <>
2024-05-17 19:23:21 +0900

    youngminIndexPage

--- src/youngmin/youngmin_index.jsx
+++ src/youngmin/youngmin_index.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 const youngmin_index = () => {
   return (
-    <div className="flex flex-row p-4 m-6 ">
+    <div className="flex flex-row p-8 m-6 ">
       <div className="bg-green-500 p-4 m-6 rounded text-white">
         <h1 className="text-blue-900">Proj.1</h1>
         <h2>살려줘요 여기 사람이 갇혀있어요</h2>
+        <p>프로젝트를 진행하면 할수록 늙어가요</p>
+        <p>전 늙기 싫어요</p>
+        <p>정시를 노릴꺼에요</p>
       </div>
       <div className="bg-green-500 p-4 m-6 rounded text-white">
         <h1>Proj.2</h1>
